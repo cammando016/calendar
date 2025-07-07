6396860b663a5074b1d99dbf6de323b77b8a3449
Cameron Anderson <cammando@hotmail.com.au>
2025-07-07 10:53:50 +1000

    Add login functionality directly in account page to display if user is not authenticated

--- frontend/src/app/(pages)/account/page.jsx
+++ frontend/src/app/(pages)/account/page.jsx
@@ -1,14 +1,19 @@
 "use client"
+import { useState } from "react";
 import { useUser } from "@/context/UserContext"
+import useLogin from "@/utils/useLogin";
+import LoginForm from "@/forms/accountForms/LoginForm";
 
 export default function Page() {
     const { user } = useUser();
+    const [loginForm, setLoginForm] = useState({username: '', password: ''});
+    const handleLogin = useLogin(loginForm, '/account');
 
     return (
         <>
             {
                 user !== null ? (
-                    <div id="logged-in-acc-details">
+                    <div id="acc-details-auth">
                         <p>First Name:</p>
                         <p>{user.firstname}</p>
 
@@ -28,7 +33,10 @@ export default function Page() {
                         <p>{user.recoveryQuestion}</p>
                     </div>
                 ) : (
-                    <p>Please login to view account details</p>
+                    <div id="acc-details-unauth">
+                        <p>Please login to view account details</p>
+                        <LoginForm form={loginForm} setForm={setLoginForm} submitFunction={handleLogin} />
+                    </div>
                 )
             }
         </>
