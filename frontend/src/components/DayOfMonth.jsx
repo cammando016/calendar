ae6e0e170c025231d84e21a1e24406afe281a08c
Cameron Anderson <cammando@hotmail.com.au>
2025-07-26 15:34:04 +1000

    Update Monthly view to display event icons on each date for events set for that date

--- frontend/src/components/DayOfMonth.jsx
+++ frontend/src/components/DayOfMonth.jsx
@@ -1,30 +1,48 @@
 //Reusable component to display each individual day in "month" view mode
-import styles from '../styles/dayofmonth.module.css'
+import styles from '../styles/dayofmonth.module.css';
+import sharedStyles from '../styles/shared.module.css';
 import { useEventList } from '@/context/EventListContext';
 
 export default function DayOfMonth({ date }) {
-    //const eventCount = 0;
     const dateObject = new Date(date);
     const { eventList } = useEventList();
 
-    const eventCount = eventList.filter(listedEvent => ((new Date(listedEvent.eventstartdate)).getDate()) === dateObject.getDate()).length;
+    const dailyEvents = eventList.filter(listedEvent => ((new Date(listedEvent.eventstartdate)).getDate()) === dateObject.getDate());
+    //console.log(dailyEvents);
 
     return (
-        <div className={`${styles.dayofmonth}`}>
-            <div className={`${styles.row} ${styles.toprow}`}>
-                {/* Display day of month */}
-                <div className="date">
-                    <p>{dateObject.getDate()}</p>
+        <div className={`${styles.dayofmonth} ${sharedStyles.rowflex}`}>
+            {/* Display dot icons for list of events user is part of for the day */}
+            {
+                dailyEvents.length > 0 && (
+                    <div className={`${sharedStyles.colflex} ${styles.eventicons}`}>
+                        {
+                            dailyEvents.slice(0,4).map(dailyEvent => {
+                                return <div key={dailyEvent.eventid} style={{backgroundColor: dailyEvent.groupcolour}} className={styles.eventicon}></div>
+                            })
+                        }
+                    </div>
+                )
+            }
+            <div className={`${styles.datedetails} ${sharedStyles.colflex}`}>
+                <div className={`${styles.row} ${styles.toprow}`}>
+                    {/* Display day of month */}
+                    <div className="date">
+                        <p>{dateObject.getDate()}</p>
+                    </div>
                 </div>
-            </div>
-            {/* Display number of events user is part of for the day */}
-            <div className={`${styles.row} ${styles.bottomrow}`}>
-                {/* Add event to selected date */}
-                <div className="add-event">
-                    <button>+</button>
+
+                <div className={`${styles.row} ${styles.bottomrow}`}>
+                    {/* Add event to selected date */}
+                    <div className="add-event">
+                        <button>+</button>
+                    </div>  
                 </div>
-                <p>{eventCount}</p>
             </div>
         </div>
     )
 }
+
+
+
+            
