2c79eaaeb76987c96311058424be929cac4001f5
Cameron Anderson <cammando@hotmail.com.au>
2025-08-16 20:54:39 +1000

    Update layout component to show account or signup button based on if user logged in

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,10 +6,6 @@ export default function Navbar ({ user, handleLogout }) {
     //Link text and assigned route for nav bar links
     const pageNav = [
         {
-            name: 'Home',
-            href: '/'
-        },
-        {
             name: 'Events',
             href: '/events'
         },
@@ -18,45 +14,68 @@ export default function Navbar ({ user, handleLogout }) {
             href: '/groups'
         },
         {
-            name: 'Account',
-            href: '/account'
+            name: 'Home',
+            href: '/'
         }
     ];
     return (
         // Navigation bar at the bottom of the page
         <div id="site-nav" className={`${styles.navbar} ${styles.layout}`}>
             {
-            pageNav.map(page => {
-                return (
-                <Link
-                    key={page.name.toLowerCase()}
-                    href={page.href}
-                    className={styles.navitem}
-                >
-                    <button><p>{page.name}</p></button>
-                </Link>
-                )
-            })
+                pageNav.map(page => {
+                    return (
+                    <Link
+                        key={page.name.toLowerCase()}
+                        href={page.href}
+                        className={styles.navitem}
+                    >
+                        <button><p>{page.name}</p></button>
+                    </Link>
+                    )
+                })
             }
             {
-            //Display login or logout option based on whether user is authenticated
-            user?.username ? (
-                <div id="nav-signout" className={styles.navitem}>
-                <button onClick={handleLogout}>
-                    <p>Log Out</p>
-                </button>
-                </div>
-            ) : (
-                <div id="nav-signin" className={styles.navitem}>
-                <Link
-                    key='signin'
-                    href='/login'
-                    className={styles.navitem}
-                >
-                    <button><p>Log In</p></button>
-                </Link>
-                </div>
-            )
+                user?.username ? (
+                    <div className={styles.navitem}>
+                        <Link 
+                            key={'account'}
+                            href={'/account'}
+                            className={styles.navitem}
+                        >
+                            <button><p>Account</p></button>
+                        </Link>
+                    </div>
+                ) : (
+                    <div className={styles.navitem}>
+                        <Link 
+                            key={'signup'}
+                            href={'/signup'}
+                            className={styles.navitem}
+                        >
+                            <button><p>Sign Up</p></button>
+                        </Link>
+                    </div>
+                )
+            }
+            {
+                //Display login or logout option based on whether user is authenticated
+                user?.username ? (
+                    <div id="nav-signout" className={styles.navitem}>
+                        <button onClick={handleLogout}>
+                            <p>Log Out</p>
+                        </button>
+                    </div>
+                ) : (
+                    <div id="nav-signin" className={styles.navitem}>
+                        <Link
+                            key='signin'
+                            href='/login'
+                            className={styles.navitem}
+                        >
+                            <button><p>Log In</p></button>
+                        </Link>
+                    </div>
+                )
             }
         </div>
     )
