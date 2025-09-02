9c94f04c5e1e793dcc732bd9b8963225d0f24a61
Cameron Anderson <cammando@hotmail.com.au>
2025-09-02 18:01:40 +1000

    Update navbar component to hide protected route links if user not signed in

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -38,82 +38,93 @@ export default function Navbar ({ user, handleLogout }) {
         // Navigation bar at the bottom of the page
         <div id="site-nav" className={`${styles.navbar} ${styles.layout}`}>
             {
-                pageNav.map(page => {
-                    return (
-                    <Link
-                        key={page.name.toLowerCase()}
-                        href={page.href}
-                        className={`${styles.navitem} ${sharedStyles.colflex}`}
-                    >
-                        <Image 
-                            src={page.imgSrc}
-                            alt={page.alt}
-                            width={30}
-                            height={30}
-                        />
-                        <p className={styles.navtext}>{page.name}</p>
-                    </Link>
-                    )
-                })
-            }
-            {
                 user?.username ? (
-                    <Link 
-                        key={'account'}
-                        href={'/account'}
-                        className={`${styles.navitem} ${sharedStyles.colflex}`}
-                    >
-                        <Image 
-                            src='/account.png'
-                            alt='link to account icon'
-                            width={30}
-                            height={30}
-                        />
-                        <p className={styles.navtext}>Account</p>
-                    </Link>
+                    <>
+                    {
+                        pageNav.map(page => {
+                            return (
+                                <Link
+                                    key={page.name.toLowerCase()}
+                                    href={page.href}
+                                    className={`${styles.navitem} ${sharedStyles.colflex}`}
+                                >
+                                    <Image 
+                                        src={page.imgSrc}
+                                        alt={page.alt}
+                                        width={30}
+                                        height={30}
+                                    />
+                                    <p className={styles.navtext}>{page.name}</p>
+                                </Link>
+                            )
+                        })
+                    }
+                        <Link 
+                            key={'account'}
+                            href={'/account'}
+                            className={`${styles.navitem} ${sharedStyles.colflex}`}
+                        >
+                            <Image 
+                                src='/account.png'
+                                alt='link to account icon'
+                                width={30}
+                                height={30}
+                            />
+                            <p className={styles.navtext}>Account</p>
+                        </Link>
+                        <button onClick={handleLogout} id="nav-signout" style={{paddingTop: 0}} className={`${styles.navitem} ${sharedStyles.colflex} ${sharedStyles.logoutbtn}`}>
+                            <Image 
+                                src='/signout.png'
+                                alt='signout icon'
+                                width={30}
+                                height={30}
+                            />
+                            <p className={styles.navtext}>Log Out</p>
+                        </button>
+                    </>
                 ) : (
-                    <Link 
-                        key={'signup'}
-                        href={'/signup'}
-                        className={`${styles.navitem} ${sharedStyles.colflex}`}
-                    >
-                        <Image 
-                            src='/signup.png'
-                            alt='link to signup page icon'
-                            width={30}
-                            height={30}
-                        />
-                        <p className={styles.navtext}>Sign Up</p>
-                    </Link>
-                )
-            }
-            {
-                //Display login or logout option based on whether user is authenticated
-                user?.username ? (
-                    <button onClick={handleLogout} id="nav-signout" style={{paddingTop: 0}} className={`${styles.navitem} ${sharedStyles.colflex} ${sharedStyles.logoutbtn}`}>
-                        <Image 
-                            src='/signout.png'
-                            alt='signout icon'
-                            width={30}
-                            height={30}
-                        />
-                        <p className={styles.navtext}>Log Out</p>
-                    </button>
-                ) : (
-                    <Link
-                        id="nav-signin" 
-                        className={`${styles.navitem} ${sharedStyles.colflex}`}
-                        key='signin'
-                        href='/login'
-                    >
-                        <Image 
-                            src='/login.png'
-                            alt='login link icon'
-                            width={30}
-                            height={30}
-                        />
-                        <p className={styles.navtext}>Log In</p>
-                    </Link>
+                    <>
+                        <Link
+                            key={'home'}
+                            href={'/'}
+                            className={`${styles.navitem} ${sharedStyles.colflex}`}
+                        >
+                            <Image
+                                src={'/home.png'}
+                                alt={'home page link'}
+                                width={30}
+                                height={30}
+                            />
+                            <p className={styles.navtext}>Home</p>
+                        </Link>
+                        <Link 
+                            key={'signup'}
+                            href={'/signup'}
+                            className={`${styles.navitem} ${sharedStyles.colflex}`}
+                        >
+                            <Image 
+                                src='/signup.png'
+                                alt='link to signup page icon'
+                                width={30}
+                                height={30}
+                            />
+                            <p className={styles.navtext}>Sign Up</p>
+                        </Link>
+                        <Link
+                            id="nav-signin" 
+                            className={`${styles.navitem} ${sharedStyles.colflex}`}
+                            key='signin'
+                            href='/login'
+                        >
+                            <Image 
+                                src='/login.png'
+                                alt='login link icon'
+                                width={30}
+                                height={30}
+                            />
+                            <p className={styles.navtext}>Log In</p>
+                        </Link>
+                    </>
                 )
             }
         </div>
