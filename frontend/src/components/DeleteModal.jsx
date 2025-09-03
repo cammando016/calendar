a1ddaea1cd4ce4d0d64f4e3d4efa30cedc8c7efb
Cameron Anderson <cammando@hotmail.com.au>
2025-09-03 20:12:54 +1000

    style delete modals

--- frontend/src/components/DeleteModal.jsx
+++ frontend/src/components/DeleteModal.jsx
@@ -1,5 +1,7 @@
 "use client"
 import { useState, useEffect, useRef } from "react"
+import styles from '../styles/utils.module.css';
+import sharedStyles from '../styles/shared.module.css';
 
 export default function DeleteModal({ closeDelete, handleDelete, deleteMessage }) {
     //Force user to click checkbox to confirm they want to delete account before delete button is enabled
@@ -32,12 +34,16 @@ export default function DeleteModal({ closeDelete, handleDelete, deleteMessage }
     }
     
     return (
-        <dialog ref={dialogRef} onCancel={handleCancelDelete} id="delete-acc-modal">
+        <dialog className={`${sharedStyles.colflex} ${sharedStyles.cardborder} ${sharedStyles.cardcolour} ${styles.modal}`} ref={dialogRef} onCancel={handleCancelDelete} id="delete-acc-modal">
             <p>{deleteMessage}</p>
-            <input type='checkbox' id="confirm-delete" onChange={toggleAllowDelete} />
-            <label htmlFor="confirm-delete">I understand deleting is irreversible and wish to proceed.</label>
-            <button onClick={handleCancelDelete}>Cancel Deletion</button>
-            <button disabled={!allowDelete} onClick={handleDelete}>Confirm Deletion</button>
+            <div>
+                <input type='checkbox' id="confirm-delete" onChange={toggleAllowDelete} />
+                <label htmlFor="confirm-delete">I understand deletion is irreversible and wish to proceed.</label>
+            </div>
+            <div className={sharedStyles.rowflex} style={{justifyContent: 'center', margin: '15px'}}>
+                <button className={`${sharedStyles.btn} ${sharedStyles.medbtn}`} type="button" onClick={handleCancelDelete}>Cancel Deletion</button>
+                <button className={`${sharedStyles.btn} ${sharedStyles.medbtn}`} type="button" disabled={!allowDelete} onClick={handleDelete}>Confirm Deletion</button>
+            </div>
         </dialog>
     )
 }
