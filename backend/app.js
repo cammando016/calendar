6b34a4a200a3a7e0b9448b020298096dd40e9186
Cameron Anderson <cammando@hotmail.com.au>
2025-09-13 00:22:57 +1000

    Add cors to backend for deployment

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,10 @@ import eventRoutes from './routes/event.js'
 dotenv.config();
 const app = express();
 
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+}));
+
 app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
